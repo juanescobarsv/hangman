fdb9811242d602305bdf505bfadd33ae1e4c0c58
Juan Escobar <juanescobar@ravn.co>
2025-05-02 16:37:04 -0600

    feat: Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+    </>
+  );
+};
+
+export default App;
+
+// function App() {
+//   return (
+//     <>
+// <h1>Hangman</h1>
+//     </>
+//   );
+// }
+
+// export default App;
